1532bcb0f7e1e7a099e25b76835bddb7a7ae13b1
antonio santos <antoniosantos.devweb@gmail.com>
2024-03-11 08:15:12 -0300

    bugs fixed

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,96 +1,98 @@
-import { useState } from 'react'
-import {View,  Text, StyleSheet, Image, TouchableOpacity, Modal} from 'react-native'
-import Slider from '@react-native-community/slider'
-import { ModalPassword} from './src/componets/modal'
 
 
-let charset = "abcdefghijklmnopqrstuvxwyzABCDEFGHIJKLMNOPQRSTUVXWYZ123456789"
-export default function  App (){
-const [size, setSize] = useState (10)
+import { useState } from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity, Modal } from "react-native";
+import Slider from "@react-native-community/slider";
+import {ModalPassword} from "../../components/modal";
 
-const [passwordValue, setPasswordValue] = useState ("")
+let charset = "abcdefghijklmnopqrstuvxwyzABCDEFGHIJKLMNOPQRSTUVXWYZ0123456789";
 
-const [modalVisible,  setModalVisible] = useState(false)
+export  function Home() {
+  const [size, setSize] = useState(10);
 
-function generatePassword (){
- let passWord = ''
- for (let i =  0 , n = charset.length; i < size ;  i++) {
-   passWord += charset.charAt(Math.floor(Math.random() * n))
- }
-setPasswordValue(passWord)
-setModalVisible(true)
+  const [passwordValue, setPasswordValue] = useState();
 
+  const [modalVisible, setModalVisible] = useState(false);
 
-}
+  function generatePassword() {
+    let password = "";
 
+    for (let i = 0, n = charset.length; i < size; i++) {
+      password += charset.charAt(Math.floor(Math.random() * n));
+    }
 
- return(
- <View style={styles.container}>
- <Image
- source={require("../home../assets/logo.png")}
- style={styles.logo}
- />
- <Text style={styles.title}>{size} ndfjdj</Text>
+    setPasswordValue(password);
+    setModalVisible(true);
+  }
 
- <View style={styles.area}>
- <Slider
- style={{height:50}}
- minimumValue={6}
- maximumValue={20}
- maximumTrackTintColor='blue'
- value={size}
- onValueChange={(velue) => setSize (velue.toFixed(0))}
- />
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../../assets/logo.png")}
+        style={styles.logo}
+      />
 
- </View>
+      <Text style={styles.title}>{size} caracteres</Text>
 
-  <TouchableOpacity style={styles.button} onPress={generatePassword}>
- <Text style={styles.buttonText}>gerar senha</Text>
-  </TouchableOpacity>
+      <View style={styles.area}>
+        <Slider
+          style={{ height: 50 }}
+          minimumValue={6}
+          maximumValue={20}
+          maximumTrackTintColor="orange"
+          minimumTrackTintColor="blue"
+          thumbTintColor="yellow"
+          value={size}
+          onValueChange={(value) => setSize(value.toFixed(0))}
+        />
+      </View>
+      <TouchableOpacity style={styles.button} onPress={generatePassword}>
+        <Text style={styles.buttonText}>Gerar Senha</Text>
+      </TouchableOpacity>
+      
+     <Modal visible={modalVisible} animationType="fade" transparent={true}>
+      <ModalPassword password={passwordValue} handleClose={() => setModalVisible(false)} />
+     </Modal>
 
-  <Modal visible={modalVisible} animationType='fade' transparent={true}>
-   < ModalPassword passWord={passwordValue} handleClose={( ) => setModalVisible(false)}/>
-  </Modal>
-
-  
- </View>
- )
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
- container:{
-   flex:1,
-   backgroundColor: "orange",
-   justifyContent: "center",
-   alignItems: "center"
- }, 
- logo:{
-   margin: 60
- },
-area:{
- marginTop:14,
- marginBottom:14,
- width: "80%",
- backgroundColor: "red",
- borderRadius: 8,
- padding: 8
- },
- button:{
-   backgroundColor: "blue",
-   borderRadius: 40,
-   width:"50%",
-   textAlign:"center",
-   alignItems: "center",
-   padding: 5,
-   marginBottom: 18
- 
- },
-buttonText:{
- color: "pink",
- fontSize: 20
-},
- title:{
-   fontSize: 20,
-   fontWeight: "bold"
- }
-})
+  container: {
+    flex: 1,
+    marginTop: 34,
+    backgroundColor: "pink",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    marginBottom: 60,
+  },
+  area: {
+    marginTop: 14,
+    marginBottom: 14,
+    width: "80%",
+    backgroundColor: "white",
+    borderRadius: 8,
+    padding: 8,
+  },
+  button: {
+    backgroundColor: "blue",
+    width: "80%",
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    marginBottom: 18,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+});
---
+++ src/pages/passwords/index.js
@@ -0,0 +1,14 @@
+import { View, Text , StyleSheet} from 'react-native'
+
+export function Passwords(){
+    return(
+        <View>
+            <Text>minhas senas</Text>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    
+})
